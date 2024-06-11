fabf008ba194e37301aa84e26b4b096fc111c358
Hui Hong Zheng <hzheng130@gmail.com>
2024-06-11 00:14:41 -0400

    write read json files

--- Day-029/main.py
+++ Day-029/main.py
@@ -3,9 +3,44 @@ from tkinter import *
 from tkinter import messagebox
 import random
 import pyperclip
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
-import random
+import json
 
+
+# ---------------------------- Search Website ------------------------------- #
+def search_website():
+    if len(web_entry.get()) == 0:
+        messagebox.showinfo(title="Error", message='Please enter the website you would like to search for')
+    else:
+        try:
+            with open('password.json', 'r') as file:
+                data = json.load(file)
+                web_info = data[web_entry.get()]
+        except IndexError:
+            messagebox.showinfo(title='Error', message='No Website information is found')
+        else:
+            messagebox.showinfo(title=f'{web_entry.get()}', message=f"Email: {web_info['email']} \n"
+                                                                    f"Password: {web_info['password']}")
+
+
+# ---------------------------- FILE INFORMATION JSON ------------------------------- #
+def file_info(new_data):
+    try:
+        with open("password.json", 'r') as file:
+            data = json.load(file)
+    except FileNotFoundError:
+        with open('password.json', 'w') as file:
+            json.dump(new_data, file, indent=4)
+    else:
+        data.update(new_data)
+        with open('password.json', 'w') as file:
+            json.dump(data, file, indent=4)
+    finally:
+        web_entry.delete(0, END)
+        email_entry.delete(0, END)
+        password_entry.delete(0, END)
+
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 def generate_password():
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
@@ -37,24 +72,27 @@ def generate_password():
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save_password():
-    if len(web_entry.get()) == 0 or len(email_entry.get()) == 0 or len(password_entry.get()):
+    if len(web_entry.get()) == 0 or len(email_entry.get()) == 0 or len(password_entry.get()) == 0:
         messagebox.showinfo(title="Error", message="Please make sure you haven't left any fields empty.")
     else:
         is_okay = messagebox.askokcancel(title=web_entry.get(),
                                          message=f"These are the details entered: \nEmail: {email_entry.get()}"
                                                  f"\nPassword: {password_entry.get()} \nIt is okay to save?")
         if is_okay:
-            with open("password.txt", 'a+') as file:
-                file.write(f"{web_entry.get()} | {email_entry.get()} | {password_entry.get()}\n")
-                web_entry.delete(0, END)
-                email_entry.delete(0, END)
-                password_entry.delete(0, END)
+            new_data = {
+                web_entry.get(): {
+                    'email': email_entry.get(),
+                    'password': password_entry.get()
+                }
+            }
+            file_info(new_data)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title = "Password Management"
+window['bg'] = 'light blue'
 window.config(pady=20, padx=50)
 
 canvas = Canvas(width=200, height=200, highlightthickness=2)
@@ -68,6 +106,9 @@ web_label.grid(column=0, row=1)
 web_entry = Entry(width=35)
 web_entry.grid(column=1, row=1, columnspan=2)
 web_entry.focus()
+
+search_button = Button(text='Search', command=search_website)
+search_button.grid(column=2, row=1, columnspan=2)
 
 email_label = Label(text='Email/Username: ')
 email_label.grid(column=0, row=2)
